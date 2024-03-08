9ef848d4fad51910c33d3be8c65aef9b2e44df28
ebcode2021 <ebcode2021@gmail.com>
2024-03-08 17:31:21 +0900

    chore:: rename root Controller

--- src/main/java/site/tellmetodo/todoapp/HomeController.java
+++ src/main/java/site/tellmetodo/todoapp/HomeController.java
@@ -1,13 +1,13 @@
-package site.tellmetodo.todoapp.global.web;
+package site.tellmetodo.todoapp;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class MainController {
+public class HomeController {
 
     @GetMapping("/")
-    public String main() {
-        return "index";
+    public String home() {
+        return "home";
     }
 }
