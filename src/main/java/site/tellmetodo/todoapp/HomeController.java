c7e61ef0ae79f557323f1022d21cb0c74fb9fe07
ebcode2021 <ebcode2021@gmail.com>
2024-03-15 17:42:36 +0900

    chore:: test

--- src/main/java/site/tellmetodo/todoapp/HomeController.java
+++ src/main/java/site/tellmetodo/todoapp/HomeController.java
@@ -1,13 +1,17 @@
 package site.tellmetodo.todoapp;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
     @GetMapping("/")
-    public String home() {
+    public String home(Model model) {
+//        model.getAttribute("todoList", {});
+//        model.addAttribute("user", "eunbi");
         return "home";
+
     }
 }
