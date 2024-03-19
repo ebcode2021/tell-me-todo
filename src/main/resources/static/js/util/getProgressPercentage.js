6affd429d7afa6e74deea39dfae2794e99f8d84a
ebcode2021 <ebcode2021@gmail.com>
2024-03-19 19:01:29 +0900

    feat:: 현재 progress 계산

---
+++ src/main/resources/static/js/util/getProgressPercentage.js
@@ -0,0 +1,26 @@
+function getProgressPercentage() {
+    console.log('여기 들어옴');
+    let checkedCount = 0;
+
+    document.querySelectorAll('.home-checkbox .checkbox-icon').forEach(function (checkBox) {
+        if (checkBox.checked) {
+            checkedCount++;
+        }
+    });
+
+    // 전체 항목 개수
+    let totalItemsCount = document.querySelectorAll('.home-todo-item').length;
+    let progressPercentage = (checkedCount / totalItemsCount) * 100;
+
+    console.log('전체 개수 : ' + totalItemsCount);
+    console.log('체크된 개수 : ' + checkedCount);
+
+    if (totalItemsCount === 0) {
+        progressPercentage = 0;
+    }
+
+    // 달성률 업데이트
+    document.querySelector('.home-progress-bar-detail').innerText = progressPercentage.toFixed(2) + '%';
+    document.querySelector('.home-progress-bar-detail').setAttribute('aria-valuenow', progressPercentage);
+    document.querySelector('.home-progress-bar-detail').style.width = progressPercentage + '%';
+}
