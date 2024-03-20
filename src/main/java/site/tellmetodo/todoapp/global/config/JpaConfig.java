f7b8ff451dd9c42b18eb5b708ced9777cd799502
ebcode2021 <ebcode2021@gmail.com>
2024-03-20 21:19:52 +0900

    feat:: @EntityListeners를 쓰기 위해 Config 추가

---
+++ src/main/java/site/tellmetodo/todoapp/global/config/JpaConfig.java
@@ -0,0 +1,9 @@
+package site.tellmetodo.todoapp.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@Configuration
+@EnableJpaRepositories
+public class JpaConfig {
+}
