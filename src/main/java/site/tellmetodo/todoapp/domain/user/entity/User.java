075c731b8d0f70d9069c9aeecff2200d12a9749e
ebcode2021 <ebcode2021@gmail.com>
2024-03-20 21:19:04 +0900

    feat:: User Entity

---
+++ src/main/java/site/tellmetodo/todoapp/domain/user/entity/User.java
@@ -0,0 +1,38 @@
+package site.tellmetodo.todoapp.domain.user.entity;
+
+import lombok.*;
+import jakarta.persistence.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "\"Users\"" )
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
+@Getter
+@EntityListeners(AuditingEntityListener.class)
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+
+    private String password;
+
+    private String nickname;
+
+    private String email;
+
+    private String bio;
+
+    @Enumerated(EnumType.STRING)
+    private UserStatus status;
+
+    @CreatedDate
+    @Column(name = "created_date")
+    private LocalDateTime createdDate;
+}
