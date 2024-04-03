ba747950cba47804042608533c0d6bd3b10367b8
ebcode2021 <ebcode2021@gmail.com>
2024-04-03 20:21:52 +0900

    feat:: JpaRepository 빈 활성화 어노테이션

--- src/main/java/site/tellmetodo/todoapp/TodoAppApplication.java
+++ src/main/java/site/tellmetodo/todoapp/TodoAppApplication.java
@@ -3,8 +3,10 @@ package site.tellmetodo.todoapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+@EnableJpaRepositories
 public class TodoAppApplication {
 
 	public static void main(String[] args) {
