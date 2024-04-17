f3e893e2365c458167f86d134cc2d4aadee01278
ebcode2021 <ebcode2021@gmail.com>
2024-04-17 16:28:02 +0900

    feat:: User, Todo table에서 사용할 BaseEntity 생성

---
+++ src/main/java/site/tellmetodo/todoapp/global/entity/BaseEntity.java
@@ -0,0 +1,32 @@
+package site.tellmetodo.todoapp.global.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
+@Getter
+public abstract class BaseEntity {
+
+    @CreatedDate // Entity 생성시 자동 저장
+    @Column(updatable = false)
+    private LocalDateTime created_at;
+
+    @LastModifiedDate
+    private LocalDateTime updated_at;
+}
+
+/*
+    순수 JPA -> @PrePersist, @PreUpdate의 기능
+    Spring JPA -> @CreatedDate, @LastModifiedDate
+
+    @MappedSuperclass
+    : BaseEntity를 상속한 엔티티들은 아래 필드를 자동으로 컬럼으로 인식
+ */
