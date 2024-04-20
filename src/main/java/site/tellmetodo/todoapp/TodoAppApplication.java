32c7d312b1b705a256e6409c429a6d30caafb0a2
ebcode2021 <ebcode2021@gmail.com>
2024-04-20 21:32:57 +0900

    delete:: SecurityAutoConfiguration

--- src/main/java/site/tellmetodo/todoapp/TodoAppApplication.java
+++ src/main/java/site/tellmetodo/todoapp/TodoAppApplication.java
@@ -2,10 +2,9 @@ package site.tellmetodo.todoapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+@SpringBootApplication
 @EnableJpaRepositories
 public class TodoAppApplication {
 
