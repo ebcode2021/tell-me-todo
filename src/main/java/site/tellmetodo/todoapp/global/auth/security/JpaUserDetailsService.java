05c9d0edeaf769731e185cfdd2e14dd6d7806fdb
ebcode2021 <ebcode2021@gmail.com>
2024-04-23 18:10:10 +0900

    feat:: JpaUserDetailsService

---
+++ src/main/java/site/tellmetodo/todoapp/global/auth/security/JpaUserDetailsService.java
@@ -0,0 +1,25 @@
+package site.tellmetodo.todoapp.global.auth.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import site.tellmetodo.todoapp.domain.user.entity.User;
+import site.tellmetodo.todoapp.domain.user.repository.UserRepository;
+
+@Service
+@RequiredArgsConstructor
+public class JpaUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        System.out.println("loadUserByUsername 들어오나요");
+        User user = userRepository
+                .findUserByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Problem during authentication!"));
+        return new UserDetailsImpl(user);
+    }
+}
