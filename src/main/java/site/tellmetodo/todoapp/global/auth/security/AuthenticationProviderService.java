3505db2af2a92fe174169f5f38c1fcfc8820dbea
ebcode2021 <ebcode2021@gmail.com>
2024-04-23 18:09:56 +0900

    feat:: AuthenticationProviderService

---
+++ src/main/java/site/tellmetodo/todoapp/global/auth/security/AuthenticationProviderService.java
@@ -0,0 +1,32 @@
+package site.tellmetodo.todoapp.global.auth.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationProviderService implements AuthenticationProvider {
+
+    private final JpaUserDetailsService userDetailsService;
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        String username = authentication.getName();
+        String password = authentication.getCredentials().toString();
+
+        UserDetailsImpl user = (UserDetailsImpl) userDetailsService.loadUserByUsername(username);
+
+        return new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword(), user.getAuthorities());
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return UsernamePasswordAuthenticationToken.class.isAssignableFrom(authentication);
+    }
+}
