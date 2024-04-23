b0c4c3ba918473081ecc616aa9418b66256bd57c
ebcode2021 <ebcode2021@gmail.com>
2024-04-23 18:15:55 +0900

    feat:: 기존 LoginPage 변경, filter 거치지 않는 경로를 WebSecurityCustomizer 사용하여 정리

---
+++ src/main/java/site/tellmetodo/todoapp/global/config/SecurityConfig.java
@@ -0,0 +1,47 @@
+package site.tellmetodo.todoapp.global.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity // 모든 요청 URL이 스프링 시큐리티의 제어를 받도록 함.
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final AuthenticationProvider authenticationProvider;
+
+    /**
+     * @brief Spring Security 설정
+     */
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authz -> authz
+                    .anyRequest().authenticated())
+                //.formLogin(AbstractHttpConfigurer::disable);
+                    .formLogin(f -> {
+                        f.loginPage("/login");
+                        f.defaultSuccessUrl("/");
+                    });
+
+        http.authenticationProvider(authenticationProvider);
+
+        return http.build();
+    }
+
+    /**
+     * @brief Spring Security filter 를 거치지 않는 경로 설정
+     */
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().requestMatchers("/css/**", "/js/**", "/svg/**", "/login");
+    }
+
+}
