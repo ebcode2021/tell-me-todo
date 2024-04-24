561785236d0d02bfe063869e384515e79a740147
ebcode2021 <ebcode2021@gmail.com>
2024-04-24 21:39:23 +0900

    feat:: DB에 있는 유저 정보와 일치여부 확인 로직

--- src/main/java/site/tellmetodo/todoapp/global/auth/security/AuthenticationProviderService.java
+++ src/main/java/site/tellmetodo/todoapp/global/auth/security/AuthenticationProviderService.java
@@ -2,9 +2,11 @@ package site.tellmetodo.todoapp.global.auth.security;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -17,11 +19,17 @@ public class AuthenticationProviderService implements AuthenticationProvider {
 
     @Override
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
-        String username = authentication.getName();
-        String password = authentication.getCredentials().toString();
+        // AuthenticationFilter 에서 생성된 토큰으로부터 아이디와 비밀번호를 조회함
+        System.out.println("authenticate 들어왔어!");
+        UsernamePasswordAuthenticationToken token = (UsernamePasswordAuthenticationToken) authentication;
+        String username = token.getName();
+        String password = (String)token.getCredentials().toString();
 
+        // DB 조회
         UserDetailsImpl user = (UserDetailsImpl) userDetailsService.loadUserByUsername(username);
-
+        if (!passwordEncoder.matches(password, user.getPassword())) {
+            throw new BadCredentialsException(user.getUsername() + "의 비밀번호가 틀렸을텐데요?");
+        }
         return new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword(), user.getAuthorities());
     }
 
