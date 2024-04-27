b21f0afa752033c73ef3e527fb1048b41c6b226a
ebcode2021 <ebcode2021@gmail.com>
2024-04-27 20:05:30 +0900

    feat:: WebSecurityCustomizer에 추가(추후 리팩해야 함)

--- src/main/java/site/tellmetodo/todoapp/global/config/SecurityConfig.java
+++ src/main/java/site/tellmetodo/todoapp/global/config/SecurityConfig.java
@@ -45,7 +45,7 @@ public class SecurityConfig {
      */
     @Bean
     public WebSecurityCustomizer webSecurityCustomizer() {
-        return (web) -> web.ignoring().requestMatchers("/css/**", "/js/**", "/svg/**", "/images/**", "/login");
+        return (web) -> web.ignoring().requestMatchers("/css/**", "/js/**", "/svg/**", "/images/**", "/login", "/join", "/users/**");
     }
 
 }
