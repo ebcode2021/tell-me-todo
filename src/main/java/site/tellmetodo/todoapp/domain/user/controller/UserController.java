5db4eaddb63adaebed25d201c20d5bb346ed2738
ebcode2021 <ebcode2021@gmail.com>
2024-04-27 14:44:35 +0900

    delete:: unused code

--- src/main/java/site/tellmetodo/todoapp/domain/user/controller/UserController.java
+++ src/main/java/site/tellmetodo/todoapp/domain/user/controller/UserController.java
@@ -1,45 +1,60 @@
 package site.tellmetodo.todoapp.domain.user.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import site.tellmetodo.todoapp.domain.user.dto.LoginUserDto;
-import site.tellmetodo.todoapp.domain.user.entity.User;
+import site.tellmetodo.todoapp.domain.user.dto.UserFormDto;
 import site.tellmetodo.todoapp.domain.user.service.UserService;
-
-import javax.swing.*;
+import site.tellmetodo.todoapp.global.auth.security.UserDetailsImpl;
 
 @Controller
-@RequestMapping("/users")
 @RequiredArgsConstructor
+@Slf4j
 public class UserController {
 
     private final UserService userService;
 
-    @PostMapping ("/")
-    public void join() {
+    @GetMapping("/")
+    public String home(@AuthenticationPrincipal UserDetailsImpl userDetails) {
+        if (userDetails == null) {
+            log.info("이게 보인다는 것은 진짜 코드가 박살난거");
+        }
+        // userDetails에 있는 유저를 넘겨줘야 함
+        return "user/home";
     }
 
-    @GetMapping("/{userId}")
+    @GetMapping("/login")
+    public String login() {
+        return "user/login";
+    }
+
+    @GetMapping("/join")
+    public String join(@ModelAttribute("userFormDto") UserFormDto userJoinDto){
+        return "user/join";
+    }
+
+    @PostMapping ("/users")
+    public void addUser(@Valid @ModelAttribute UserFormDto userJoinDto) {
+    }
+
+    @GetMapping("/users/{userId}")
     public void getUser(@PathVariable String userId) {
 
     }
 
-    @PostMapping("/login")
-    public String login(@ModelAttribute LoginUserDto loginUser) {
-        User user = userService.findUserByUsername(loginUser.getUsername());
-        if (user != null) {
-            System.out.println(user.toString());
-        } else {
-            System.out.println("유저 정보 없음");
-        }
-        return "redirect:/";
-    }
 
-    @PostMapping("/logout")
-    public void logout() {
+    @PostMapping("/users/logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response) {
+        new SecurityContextLogoutHandler().logout(request, response, SecurityContextHolder.getContext().getAuthentication());
 
+        return "redirect:/login";
     }
 
 }
