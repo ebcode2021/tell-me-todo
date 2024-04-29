2c5e90352f1978b81eac361993ef0776f24c86a0
ebcode2021 <ebcode2021@gmail.com>
2024-04-29 17:14:17 +0900

    update:: Password 암호화 적용(NoOpPasswordEncoder -> BCryptPasswordEncoder)

--- src/main/java/site/tellmetodo/todoapp/global/config/PasswordConfig.java
+++ src/main/java/site/tellmetodo/todoapp/global/config/PasswordConfig.java
@@ -2,6 +2,7 @@ package site.tellmetodo.todoapp.global.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -19,6 +20,6 @@ public class PasswordConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
+        return new BCryptPasswordEncoder();
     }
 }
