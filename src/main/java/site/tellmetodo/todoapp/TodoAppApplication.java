657af64a00d8cd95f8e8c11390695ff692ad50e4
ebcode2021 <ebcode2021@gmail.com>
2024-04-29 17:13:20 +0900

    feat:: BaseRepository 감지를 위한 어노테이션 추가

--- src/main/java/site/tellmetodo/todoapp/TodoAppApplication.java
+++ src/main/java/site/tellmetodo/todoapp/TodoAppApplication.java
@@ -2,10 +2,12 @@ package site.tellmetodo.todoapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories
+@EnableJpaAuditing
 public class TodoAppApplication {
 
 	public static void main(String[] args) {
