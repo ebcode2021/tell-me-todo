f57b4254c0b6c197d1330b91abf5d0144f9f364a
ebcode2021 <ebcode2021@gmail.com>
2024-05-02 20:44:19 +0900

    update:: import할 때 자동 실행되도록 변경

--- src/main/resources/static/js/util/getCurrentDate.js
+++ src/main/resources/static/js/util/getCurrentDate.js
@@ -4,3 +4,5 @@ function getCurrentDate() {
     document.getElementById('current-date').innerText =
         `${currentDate.getFullYear()}년 ${currentDate.getMonth() + 1}월 ${currentDate.getDate()}일`;
 }
+
+getCurrentDate();
