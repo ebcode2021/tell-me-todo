69322f4c0c6c7e5d9da9c44032755157556674c2
ebcode2021 <ebcode2021@gmail.com>
2024-05-03 21:52:35 +0900

    feat:: entity -> dto 변환

--- src/main/java/site/tellmetodo/todoapp/domain/todo/dto/TodoListDto.java
+++ src/main/java/site/tellmetodo/todoapp/domain/todo/dto/TodoListDto.java
@@ -1,18 +1,31 @@
 package site.tellmetodo.todoapp.domain.todo.dto;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
+import site.tellmetodo.todoapp.domain.todo.entity.Todo;
+
+import java.time.LocalDateTime;
 
 @Getter @Setter
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class TodoListDto {
-
-    private Long id;
-
-    private Long userId;
 
     private String content;
 
     private boolean favorite;
 
     private boolean completed;
+
+    private LocalDateTime created_at;
+
+    private LocalDateTime updated_at;
+
+    public static TodoListDto toDto(Todo todo) {
+        return new TodoListDto(
+                todo.getContent(),
+                todo.isFavorite(),
+                todo.isCompleted(),
+                todo.getCreated_at(),
+                todo.getUpdated_at()
+        );
+    }
 }
