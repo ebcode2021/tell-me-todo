c1c92680f8fb8769524cd2d87babefd68c3de610
ebcode2021 <ebcode2021@gmail.com>
2024-05-03 21:53:07 +0900

    feat:: queryDsl을 사용한 Repoisotry

--- src/main/java/site/tellmetodo/todoapp/domain/todo/repository/CustomizedTodoRepository.java
+++ src/main/java/site/tellmetodo/todoapp/domain/todo/repository/CustomizedTodoRepository.java
@@ -1,6 +1,12 @@
 package site.tellmetodo.todoapp.domain.todo.repository;
 
-import site.tellmetodo.todoapp.domain.user.repository.CustomizedUserRepository;
+import site.tellmetodo.todoapp.domain.todo.dto.TodoListDto;
+import site.tellmetodo.todoapp.domain.todo.entity.Todo;
+
+import java.time.LocalDate;
+import java.util.List;
 
 public interface CustomizedTodoRepository {
+
+    List<Todo> findTodoListByUserIdAndDate(Long id, LocalDate date);
 }
--- src/main/java/site/tellmetodo/todoapp/domain/todo/repository/CustomizedTodoRepositoryImpl.java
+++ src/main/java/site/tellmetodo/todoapp/domain/todo/repository/CustomizedTodoRepositoryImpl.java
@@ -1,5 +1,33 @@
 package site.tellmetodo.todoapp.domain.todo.repository;
 
+import com.querydsl.core.types.dsl.Expressions;
+import com.querydsl.core.types.dsl.StringExpression;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+import site.tellmetodo.todoapp.domain.todo.dto.TodoListDto;
+import site.tellmetodo.todoapp.domain.todo.entity.Todo;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static site.tellmetodo.todoapp.domain.todo.entity.QTodo.todo;
+
+@Repository
+@RequiredArgsConstructor
 public class CustomizedTodoRepositoryImpl implements CustomizedTodoRepository {
+
+    private final JPAQueryFactory jpaQueryFactory;
+
+    @Override
+    public List<Todo> findTodoListByUserIdAndDate(Long id, LocalDate date) {
+        StringExpression formattedDate = Expressions.stringTemplate("TO_CHAR({0}, 'YYYY-MM-DD')", todo.created_at);
+
+        return jpaQueryFactory.selectFrom(todo)
+                .where(todo.user.id.eq(id)
+                        .and(formattedDate.eq(date.toString())))
+                .fetch();
+    }
 }
 
