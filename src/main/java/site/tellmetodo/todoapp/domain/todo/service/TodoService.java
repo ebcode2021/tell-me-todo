0a63ace483531ed676ac43e3b9769f3554addc3a
ebcode2021 <ebcode2021@gmail.com>
2024-05-03 21:53:54 +0900

    feat:: getTodoListByUserIdAndLocalDate method

--- src/main/java/site/tellmetodo/todoapp/domain/todo/service/TodoService.java
+++ src/main/java/site/tellmetodo/todoapp/domain/todo/service/TodoService.java
@@ -2,11 +2,11 @@ package site.tellmetodo.todoapp.domain.todo.service;
 
 import site.tellmetodo.todoapp.domain.todo.dto.TodoListDto;
 
-import java.util.Date;
+import java.time.LocalDate;
 import java.util.List;
 
 public interface TodoService {
 
-    List<TodoListDto> getTodoList(Long id, Date date);
+    List<TodoListDto> getTodoListByUserIdAndLocalDate(Long id, LocalDate date);
 
 }
--- src/main/java/site/tellmetodo/todoapp/domain/todo/service/TodoServiceImpl.java
+++ src/main/java/site/tellmetodo/todoapp/domain/todo/service/TodoServiceImpl.java
@@ -1,18 +1,31 @@
 package site.tellmetodo.todoapp.domain.todo.service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import site.tellmetodo.todoapp.domain.todo.dto.TodoListDto;
+import site.tellmetodo.todoapp.domain.todo.entity.Todo;
+import site.tellmetodo.todoapp.domain.todo.repository.TodoRepository;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.Date;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class TodoServiceImpl implements TodoService{
 
+    private final TodoRepository todoRepository;
+
     @Override
-    public List<TodoListDto> getTodoList(Long id, Date date) {
-        return null;
+    public List<TodoListDto> getTodoListByUserIdAndLocalDate(Long id, LocalDate date) {
+        List<Todo> todoList = todoRepository.findTodoListByUserIdAndDate(id, date);
+
+        return todoList.stream()
+                .map(TodoListDto::toDto)
+                .collect(Collectors.toList());
     }
 }
