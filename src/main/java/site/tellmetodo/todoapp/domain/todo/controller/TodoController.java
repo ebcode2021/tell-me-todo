214a90f29a1b8816d354ebd11e6eacdf1daefdc9
ebcode2021 <ebcode2021@gmail.com>
2024-05-03 21:52:15 +0900

    chore:: Controller로 들어오지 않을 것 같아서 일단 주석

--- src/main/java/site/tellmetodo/todoapp/domain/todo/controller/TodoController.java
+++ src/main/java/site/tellmetodo/todoapp/domain/todo/controller/TodoController.java
@@ -6,10 +6,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import site.tellmetodo.todoapp.domain.todo.dto.TodoListDto;
 import site.tellmetodo.todoapp.domain.todo.service.TodoService;
 import site.tellmetodo.todoapp.global.auth.security.UserDetailsImpl;
 
+import java.time.LocalDateTime;
 import java.util.Date;
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -18,11 +21,12 @@ public class TodoController {
 
     private final TodoService todoService;
 
-    @GetMapping("/date/{date}")
-    public void getTodosByDate(@AuthenticationPrincipal UserDetailsImpl userDetails, @PathVariable("date") Date date) {
-        Long id = userDetails.getUser().getId();
-
-    }
+//    @GetMapping("/date/{date}")
+//    public List<TodoListDto> getTodosByDate(@AuthenticationPrincipal UserDetailsImpl userDetails, @PathVariable("date") LocalDateTime date) {
+//        Long id = userDetails.getUser().getId();
+//
+//        return todoService.getTodoListByUserIdAndDate(id, date);
+//    }
 //    @GetMapping
 //    public String getTodoList(@AuthenticationPrincipal UserDetailsImpl userDetails) {
 //        System.out.println(userDetails.getUsername());
