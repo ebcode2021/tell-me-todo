94ce1a72b29fc3eacbe1a935c1f06a848dcd5475
ebcode2021 <ebcode2021@gmail.com>
2024-05-05 23:19:24 +0900

    chore:: 데이터 수정

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,4 +2,4 @@ INSERT INTO "User" (id, username, password, nickname, email, bio, status, create
 VALUES (100, 'eunson', '$2a$10$JJtPo4mrBAJcYgS/ZvFT1.Jel9wbncn7191Owpl322.R3/jo7rVKi', '은비땽', 'ebcode2021@gmail.com', 'This is a test user', 'ACTIVE', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
 
 INSERT INTO "Todo" (id, user_id, content, favorite, completed, created_at, updated_at)
-VALUES (1000, 100, '안녕', true, false, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+VALUES (1000, 100, '안녕', true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
