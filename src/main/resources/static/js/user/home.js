7bb31ae18923069744e4077adf3d641b1fe0803d
ebcode2021 <ebcode2021@gmail.com>
2024-05-07 17:11:55 +0900

    feat:: Validation을 위한 checkContentLength

--- src/main/resources/static/js/user/home.js
+++ src/main/resources/static/js/user/home.js
@@ -2,11 +2,22 @@
  * Fetch Function
  */
 const clearTodoList = async() => {
+    if (document.querySelector(".home-todo-item").length === 0)
+        return;
+
     const date = new Date().toISOString().split('T')[0];
     await deleteTodoListForDate(date);
     window.location.reload();
 }
 
+/**
+ * Validate Function
+ */
+const checkContentLength = () => {
+    const content = document.querySelector("#add-todo-input").value;
+
+    return content.trim().length !== 0;
+}
 
 /**
  * Event Function
