cdd4e23c7af519232069919b62b16295cc76f25a
ebcode2021 <ebcode2021@gmail.com>
2024-05-08 14:19:48 +0900

    feat:: 타임리프를 이용한 API 호출 -> 이벤트 처리를 위해 직접 JS 작성

--- src/main/resources/static/js/api/todo-api.js
+++ src/main/resources/static/js/api/todo-api.js
@@ -2,7 +2,40 @@ const deleteTodoListForDate = async(date) => {
     try {
         await sendApiRequest(
             'DELETE',
-            "/todos/" + date,
+            "/todos/date/" + date,
+        )
+    } catch(e) {
+        console.error(e);
+    }
+}
+
+const updateTodoCompleted = async(id) => {
+    try {
+        await sendApiRequest(
+            'PATCH',
+            "/todos/" + id + "/completed"
+        )
+    } catch (e) {
+        console.error(e);
+    }
+}
+
+const updateTodoFavorite = async(id) => {
+    try {
+        await sendApiRequest(
+            'PATCH',
+            "/todos/" + id + "/favorite",
+        )
+    } catch (e) {
+        console.error(e);
+    }
+}
+
+const deleteTodo = async(id) => {
+    try {
+        await sendApiRequest(
+            'DELETE',
+            "/todos/" + id,
         )
     } catch(e) {
         console.error(e);
