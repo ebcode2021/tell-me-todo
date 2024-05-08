599c7008153047947614059984abf0da79d796d6
ebcode2021 <ebcode2021@gmail.com>
2024-05-08 14:19:54 +0900

    feat:: 타임리프를 이용한 API 호출 -> 이벤트 처리를 위해 직접 JS 작성

--- src/main/resources/static/js/user/home.js
+++ src/main/resources/static/js/user/home.js
@@ -2,12 +2,44 @@
  * Fetch Function
  */
 const clearTodoList = async() => {
-    if (document.querySelector(".home-todo-item").length === 0)
+    const todoItems = document.querySelectorAll(".home-todo-item");
+    if (todoItems.length === 0)
         return;
 
     const date = new Date().toISOString().split('T')[0];
     await deleteTodoListForDate(date);
-    window.location.reload();
+
+    todoItems.forEach(item => {
+        item.remove();
+    })
+    setZeroProgress();
+}
+
+const clickCheckbox = async(id) => {
+    await updateTodoCompleted(id);
+    getProgressPercentage();
+}
+
+const toggleFavorite = async(id) => {
+    await updateTodoFavorite(id);
+
+    const image = document.querySelector(`#favorite_${id}`);
+
+    if (image.src.endsWith('star-fill.svg')) {
+        image.src = '/images/svg/star.svg';
+    } else {
+        image.src = '/images/svg/star-fill.svg';
+    }
+}
+
+const clearTodo = async(id) => {
+    await deleteTodo(id);
+
+    const item = document.getElementById(`home-item-${id}`);
+    if (item) {
+        item.remove();
+    }
+    getProgressPercentage();
 }
 
 /**
